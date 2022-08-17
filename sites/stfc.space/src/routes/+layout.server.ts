b17dcde490401232da0e5d95d0bec69732ffc632
Ripper <ripper@stfc.space>
2022-08-17 14:32:11 -0700

    Do the logout url stuff on the server

--- sites/stfc.space/src/routes/+layout.server.ts
+++ sites/stfc.space/src/routes/+layout.server.ts
@@ -3,6 +3,15 @@ import { pick } from '$lib/shared/acceptLanguageParse';
 
 import type { LayoutServerLoad } from './$types';
 
+async function getLogoutUrl(request: Request) {
+  const r = await fetch(`/api/auth/self-service/logout/browser`, {
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  return (await r.json<{ logout_url: string }>()).logout_url;
+}
+
 export const load: LayoutServerLoad = async ({ request, locals }) => {
   const cookies = libCookie.parse(request.headers.get('cookie') || '');
   const theme = cookies.theme ?? 'dark';
@@ -15,7 +24,7 @@ export const load: LayoutServerLoad = async ({ request, locals }) => {
     user: userId
       ? {
           id: userId,
-          logoutUrl: null
+          logoutUrl: await getLogoutUrl(request)
         }
       : null
   };
--- sites/stfc.space/src/routes/+layout.ts
+++ sites/stfc.space/src/routes/+layout.ts
@@ -6,33 +6,10 @@ import { setupSearch, waitSearchReady } from '$lib/shared/search';
 import { waitStaticData } from '$lib/shared/api';
 import type { LayoutLoad } from './$types';
 
-async function getLogoutUrl(fetch: Window['fetch'], user: unknown) {
-  if (!user) {
-    return null;
-  }
-
-  const r = await fetch(`/api/auth/self-service/logout/browser`, {
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-  const r2 = await r.json<{ logout_url: string }>();
-  return r2?.logout_url;
-}
-
 export const load: LayoutLoad = async ({ data, fetch }) => {
   const { lang, user, theme } = data;
   setupI18n({ withLocale: lang }, fetch);
   setupSearch();
-  let logoutUrl: string | undefined;
-  await Promise.all([
-    waitLocale(lang),
-    waitSearchReady(lang, fetch),
-    waitStaticData(fetch),
-    getLogoutUrl(fetch, user).then((url) => {
-      logoutUrl = url;
-    })
-  ]);
-
-  return { theme: theme, lang: lang, logoutUrl };
+  await Promise.all([waitLocale(lang), waitSearchReady(lang, fetch), waitStaticData(fetch)]);
+  return { theme: theme, lang: lang, logoutUrl: user.logoutUrl };
 };
